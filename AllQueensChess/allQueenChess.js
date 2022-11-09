f78c03a5cc4275b4c27978308ac3e9638394a56c
stevne.chao <gash612@gmail.com>
2022-11-09 08:09:03 +0800

    [AQC][Feat] Chess movement without check

--- AllQueensChess/allQueenChess.js
+++ AllQueensChess/allQueenChess.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             lastStepChessboard: [],
             chosen: [0, 0],
             isChoosing: false,
+            playerTurn: "A",
         };
     },
     mounted() {
@@ -29,8 +30,35 @@ const app = Vue.createApp({
             }
         },
         selectCell(lineIndex, cellIndex) {
+            if (this.getPosInfo(lineIndex, cellIndex) != this.playerTurn && !this.isChoosing) return;
+            if (this.isChoosing) {
+                if (this.getPosInfo(lineIndex, cellIndex) == this.playerTurn) {
+                    this.chosen = [lineIndex, cellIndex];
+                    this.isChoosing = true;
+                    return;
+                }
+                if (this.isValidStep(this.chosen, [lineIndex, cellIndex])) {
+                    this.chessboard[this.chosen[0]][this.chosen[1]] = '-';
+                    this.chessboard[lineIndex][cellIndex] = this.playerTurn;
+                    this.isChoosing = false;
+                    this.playerTurn = this.playerTurn == 'A' ? 'B' : 'A';
+                    return;
+                }
+                return;
+            }
             this.chosen = [lineIndex, cellIndex];
             this.isChoosing = true;
+        },
+        isValidStep(lastPos, newPos) {
+            let oldX = lastPos[0];
+            let oldY = lastPos[1];
+            let newX = newPos[0];
+            let newY = newPos[1];
+            if (this.getPosInfo(newX, newY) != '-') return false;
+            return true;
+        },
+        getPosInfo(lineIndex, cellIndex) {
+            return this.chessboard[lineIndex][cellIndex];
         }
     }
 });
