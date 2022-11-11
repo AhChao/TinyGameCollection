04c93cf90589a95326542a76967fb010b8ca3fb8
stevne.chao <gash612@gmail.com>
2022-11-12 00:27:44 +0800

    [OTB][Feat] Add valid logic for sheep on the board

--- OnTheBridge/onTheBridge.js
+++ OnTheBridge/onTheBridge.js
@@ -99,7 +99,12 @@ const app = Vue.createApp({
             if (this.chosen[0] < 0) {
                 let validCellIndex = this.getTheFirstDifferentBackgroundCellIndex(this.chosen[0] == -1 ? 1 : 4, this.getPosInfo(this.chosen[0], this.chosen[1])[1], this.chosen[0] == -1 ? 1 : -1);
                 this.validCell = [this.getTopEmptyIndexOfTheColumn(validCellIndex), validCellIndex];
+                return;
             }
+            let chosenInfo = this.getPosInfo(this.chosen[0], this.chosen[1]);
+            let dir = chosenInfo[0] == 'W' ? 1 : -1;
+            let validCellIndex = this.getTheFirstDifferentBackgroundCellIndex(this.chosen[1] + dir, chosenInfo[1], dir);
+            this.validCell = [this.getTopEmptyIndexOfTheColumn(validCellIndex), validCellIndex];
         },
         getTheFirstDifferentBackgroundCellIndex(startIndex, backgroundColor, dir) {
             for (let i = startIndex; (dir == 1 ? i < 5 : i > 0); i += dir) {
