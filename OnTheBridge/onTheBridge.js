e5e42f8d0f947a65b6eee7f6bd82dba607576dfd
stevne.chao <gash612@gmail.com>
2022-11-11 08:41:19 +0800

    [OTB][Feat] Init sheep in the waiting place

--- OnTheBridge/onTheBridge.js
+++ OnTheBridge/onTheBridge.js
@@ -8,23 +8,47 @@ const app = Vue.createApp({
             chosen: [0, 0],
             isChoosing: false,
             playerTurn: "W",
+            waitingPlaceW: [],
+            waitingPlaceB: [],
         };
     },
     mounted() {
+        this.init();
     },
     computed: {
     },
     methods: {
         init() {
-            rollForSheeps();
+            this.rollForSheeps();
             this.lastStepChessboard = [];
             this.validCell = [];
             this.isChoosing = false;
-            this.playerTurn = "A";
+            this.playerTurn = "b";
             this.drawField();
         },
-        rollForSheeps() {
+        drawField() {
 
+        },
+        rollForSheeps() {
+            let wbCount = Math.floor(Math.random() * 3) + 1;
+            let bbCount = Math.floor(Math.random() * 3) + 1;
+            for (let i = 0; i < 5; i++) {
+                if (wbCount > 0) {
+                    wbCount--;
+                    this.waitingPlaceW.push("WB");
+                }
+                else {
+                    this.waitingPlaceW.push("WW");
+                }
+                if (bbCount > 0) {
+                    bbCount--;
+                    this.waitingPlaceB.push("BB");
+                }
+                else {
+                    this.waitingPlaceB.push("BW");
+                }
+                console.log(this.waitingPlaceB);
+            }
         }
     }
 });
