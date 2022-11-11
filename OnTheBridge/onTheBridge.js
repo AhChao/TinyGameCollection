962f396bca6367b7dc1bd65b6cf47cd7826e4fc0
stevne.chao <gash612@gmail.com>
2022-11-12 00:12:41 +0800

    [OTB][Feat] White sheep move from waiting place

--- OnTheBridge/onTheBridge.js
+++ OnTheBridge/onTheBridge.js
@@ -56,20 +56,71 @@ const app = Vue.createApp({
 
         },
         getPosInfo(lineIndex, cellIndex) {
-            return this.chessboard[lineIndex, cellIndex];
+            if (lineIndex < 0) {
+                if (lineIndex == -1) return this.waitingPlaceW[cellIndex];
+                if (lineIndex == -2) return this.waitingPlaceB[cellIndex];
+            }
+            return this.chessboard[lineIndex][cellIndex];
         },
         selectCell(lineIndex, cellIndex) {
-            let selectedCell = "";
-            if (lineIndex < 0) {
-                if (lineIndex == -1) selectedCell = this.waitingPlaceW[cellIndex];
-                if (lineIndex == -2) selectedCell = this.waitingPlaceB[cellIndex];
+            let selectedCell = this.getPosInfo(lineIndex, cellIndex);
+            if (this.isChoosing == false) {
+                if (lineIndex == 0 || lineIndex == 5) return;
+                if (selectedCell[0] != this.playerTurn) return;
+                this.isChoosing = true;
+                this.chosen = [lineIndex, cellIndex];
+                this.updateValidCell();
             }
-            else selectedCell = this.getPosInfo(lineIndex, cellIndex);
-            if (selectedCell[0] != this.playerTurn) return;
-            this.isChoosing = true;
-            this.chosen = [lineIndex, cellIndex];
+            else {
+                if (selectedCell[0] == this.playerTurn) {
+                    this.chosen = [lineIndex, cellIndex];
+                    return;
+                }
+                if (selectedCell[0] != '-') return;
+                if (lineIndex == this.validCell[0] && cellIndex == this.validCell[1]) {
+                    if (this.chosen[0] < 0) {//select from waiting place
+                        this.chessboard[lineIndex][cellIndex] = this.getPosInfo(this.chosen[0], this.chosen[1]);
+                        if (this.chosen[0] == -1) this.waitingPlaceW.splice(this.chosen[1], 1);
+                        if (this.chosen[0] == -2) this.waitingPlaceB.splice(this.chosen[1], 1);
+                    }
+                    else {//swtich all sheep above the moving one
+                        let dir = this.chosen[1]
+                        let topNonEmtpyLine = this.getTopNonEmptyIndexOfTheColumn(this.chosen[1]);
 
+
+                    }
+                    this.isChoosing = false;
+                }
+
+            }
+        },
+        updateValidCell() {
+            if (this.chosen[0] == -1) {
+                let validCellIndex = this.getTheFirstDifferentBackgroundCellIndex(1, this.getPosInfo(this.chosen[0], this.chosen[1]), 1);
+                this.validCell = [this.getTopEmptyIndexOfTheColumn(validCellIndex), validCellIndex];
+            }
+        },
+        getTheFirstDifferentBackgroundCellIndex(startIndex, backgroundColor, dir) {
+            for (let i = startIndex; (dir == 1 ? i < 5 : i > 0); i += dir) {
+                if (this.getPosInfo(this.getTopNonEmptyIndexOfTheColumn(i), i) != backgroundColor) {
+                    return i;
+                }
+            }
+            return dir == 1 ? 5 : 0;
+        },
+        getTopNonEmptyIndexOfTheColumn(cellIndex) {
+            for (let i = 0; i < 9; i++) {
+                if (this.getPosInfo(i, cellIndex) != '-') return i;
+            }
+            return 9;
+        },
+        getTopEmptyIndexOfTheColumn(cellIndex) {
+            for (let i = 9; i > 0; i--) {
+                if (this.getPosInfo(i, cellIndex) == '-') return i;
+            }
+            return 0;
         }
+
     }
 });
 app.mount('#app');
