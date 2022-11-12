e5cc61c5b141c2b98dabcca1980edddaff7c3d71
stevne.chao <gash612@gmail.com>
2022-11-12 09:37:55 +0800

    [OTB][Feat] Bring back home case

--- OnTheBridge/onTheBridge.js
+++ OnTheBridge/onTheBridge.js
@@ -88,7 +88,7 @@ const app = Vue.createApp({
                             [this.chessboard[lineI][this.chosen[1]], this.chessboard[lineIndex][cellIndex]] = [this.chessboard[lineIndex][cellIndex], this.chessboard[lineI][this.chosen[1]]];
                         }
                         //If send enemy's sheep to your home, trigger option to let the enemy do chossing
-
+                        this.checkBringBackHome();
                     }
                     this.isChoosing = false;
                     this.playerTurn = this.playerTurn == 'W' ? 'B' : 'W';
@@ -126,8 +126,25 @@ const app = Vue.createApp({
                 if (this.getPosInfo(i, cellIndex) == '-') return i;
             }
             return 0;
+        },
+        checkBringBackHome() {
+            let lineIdList = [];
+            let checkingCellId = this.playerTurn == 'W' ? 5 : 0;
+            for (let i = 0; i < 10; i++) {
+                if (this.playerTurn == 'W' && this.getPosInfo(i, 5)[0] == 'B') lineIdList.push(i);
+                if (this.playerTurn == 'B' && this.getPosInfo(i, 0)[0] == 'W') lineIdList.push(i);
+            }
+            if (lineIdList.length == 0) return;
+            let message = "Player " + (this.playerTurn == 'W' ? 'B' : 'W') + "'s Sheep was bring back to grass!\nChoose which background of sheep you want.\nOk to be White, Cancel to be Black."
+            for (let i = 0; i < lineIdList.length; i++) {
+                this.chessboard[lineIdList[i]][checkingCellId] = '-';
+                let chessName = this.playerTurn == 'W' ? "B" : "W";//if player W time, only will send back B sheep
+                if (confirm(message)) chessName += "W";
+                else chessName += "B";
+                if (this.playerTurn == 'W') this.waitingPlaceB.push(chessName);
+                else this.waitingPlaceW.push(chessName);
+            }
         }
-
     }
 });
 app.mount('#app');
