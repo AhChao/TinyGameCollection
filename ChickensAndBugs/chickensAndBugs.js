1c46ce06aa61beba48b1c166eb420abc4d643a36
stevne.chao <gash612@gmail.com>
2022-11-13 23:57:01 +0800

    [CAB][Feat] Place phase

--- ChickensAndBugs/chickensAndBugs.js
+++ ChickensAndBugs/chickensAndBugs.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
     created() { },
     data() {
         return {
-            playerTurn: "1",
+            playerTurn: "0",
             boardArray: [],
             playerCount: 2, // 2-4, 3-3, 4-2
+            placedChicken: 0,
+            shouldPlacedChicken: 8,
             playerScore: [0, 0],
+            placePhase: true,
+            chickenPlace: [],
         };
     },
     mounted() {
@@ -18,8 +22,11 @@ const app = Vue.createApp({
             this.boardArray = [];//7-8-7, worm 1*30, 2*20, 3*10
             this.drawField();
             this.playerCount = 2;//TODO:fix as 2 for now, max should support 4
+            this.placedChicken = 0;
+            this.shouldPlacedChicken = (6 - this.playerCount) * this.playerCount;
+            this.chickenPlace = [[[-1, -1], [-1, -1], [-1, -1], [-1, -1]], [[-1, -1], [-1, -1], [-1, -1], [-1, -1]]];
             this.playerScore = [0, 0];
-
+            this.placePhase = true;
         },
         drawField() {
             let wormOrder = [];
@@ -36,7 +43,45 @@ const app = Vue.createApp({
                     this.boardArray[i].push(wormOrder.pop());
                 }
             }
-            console.log(this.boardArray);
+        },
+        selectCell(lineIndex, cellIndex) {
+            if (this.placePhase) {
+                if (!this.checkPosAbleToStand(lineIndex, cellIndex)) return;
+                if (this.boardArray[lineIndex][cellIndex] != 1) return;//only can start from cell with one worm
+                for (let i = 0; i < this.chickenPlace[this.playerTurn].length; i++) {
+                    if (this.chickenPlace[this.playerTurn][i][0] == -1) {
+                        this.chickenPlace[this.playerTurn][i][0] = lineIndex;
+                        this.chickenPlace[this.playerTurn][i][1] = cellIndex;
+                        this.placedChicken++;
+                        if (this.placedChicken == this.shouldPlacedChicken) this.placePhase = false;
+                        this.nextPlayer();
+                        break;
+                    }
+                }
+
+            }
+            else {
+
+            }
+        },
+        getCoodX(lineIndex, cellIndex) {
+            return cellIndex * 80 + (lineIndex % 2 == 0 ? 40 : 0);
+        },
+        getCoodY(lineIndex) {
+            return lineIndex * 61;
+        },
+        checkPosAbleToStand(lineIndex, cellIndex) {
+            for (let i = 0; i < this.chickenPlace; i++) {
+                for (let j = 0; j < this.chickenPlace[i]; j++) {
+                    let place = this.chickenPlace[i][j];
+                    if (place[0] == lineIndex && place[1] == cellIndex) return false;
+                }
+            }
+            return true;
+        },
+        nextPlayer() {
+            this.playerTurn = this.playerTurn * 1 + 1;
+            if (this.playerTurn == this.playerCount) this.playerTurn = 0;
         }
     }
 });
