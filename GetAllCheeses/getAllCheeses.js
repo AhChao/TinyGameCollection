114f1d0361ccd4333be7a675f8752d24e91dfd33
stevne.chao <gash612@gmail.com>
2022-12-19 18:41:08 +0800

    [GAC][Feat] Roll mutli dices

--- GetAllCheeses/getAllCheeses.js
+++ GetAllCheeses/getAllCheeses.js
@@ -8,9 +8,13 @@ const app = Vue.createApp({
             validCell: [],
             chosen: [0, 0],
             isChoosing: false,
-            playerTurn: "A",
+            playerTurn: 1,//1 - n
+            playerCount: 2,
             randomMin: 1,
-            randomMax: 30
+            randomMax: 30,
+            diceCount: 13,
+            remainDiceCount: [13],
+            rollResult: {},
         };
     },
     mounted() {
@@ -23,20 +27,31 @@ const app = Vue.createApp({
             this.lastStepChessboard = [];
             this.validCell = [];
             this.isChoosing = false;
-            this.playerTurn = "A";
-            this.drawField();
+            this.playerTurn = 1;
         },
         getRandom(max, min) {
             return (Math.floor(Math.random() * (max - min)) + min) * 90;
         },
-        rollTheDice(id) {
-            console.log(id);
-            let cube = document.getElementById(id);
-            let xRand = this.getRandom(this.randomMax, this.randomMin);
-            let yRand = this.getRandom(this.randomMax, this.randomMin);
-            cube.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-            cube.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
-            this.WonTheGameBy(this.checkDiceFace(xRand, yRand));
+        rollTheDices() {
+            let diceId = "";
+            let cube = "";
+            let xRand = 0;
+            let yRand = 0;
+            let singleResult = "";
+            this.rollResult = {};
+            for (let i = 1; i <= this.remainDiceCount[this.playerTurn - 1]; i++) {
+                diceId = "dice" + i;
+                cube = document.getElementById(diceId);
+                xRand = this.getRandom(this.randomMax, this.randomMin);
+                yRand = this.getRandom(this.randomMax, this.randomMin);
+                cube.style.webkitTransform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+                cube.style.transform = 'rotateX(' + xRand + 'deg) rotateY(' + yRand + 'deg)';
+                singleResult = this.checkDiceFace(xRand, yRand);
+                if (!(singleResult in this.rollResult)) {
+                    this.rollResult[singleResult] = 0;
+                }
+                this.rollResult[singleResult]++;
+            }
         },
         checkDiceFace(degX, degY) {
             if (degX % 360 == 0) {
@@ -71,159 +86,12 @@ const app = Vue.createApp({
             }
 
         },
-        drawField() {
-            for (let i = 0; i < 5; i++) {
-                this.chessboard[i] = ['-', '-', '-', '-', '-'];
-                for (let j = 0; j < 5; j++) {
-                    if (i == 0) { this.chessboard[i][j] = j % 2 == 0 ? 'B' : 'A'; }
-                    else if (i == 4) { this.chessboard[i][j] = j % 2 == 0 ? 'A' : 'B'; }
-                    else if (i == 2 && j == 0) { this.chessboard[i][j] = 'A' }
-                    else if (i == 2 && j == 4) { this.chessboard[i][j] = 'B' }
-                }
-            }
-        },
-        selectCell(lineIndex, cellIndex) {
-            if (this.getPosInfo(lineIndex, cellIndex) != this.playerTurn && !this.isChoosing) return;
-            if (this.isChoosing) {
-                if (this.getPosInfo(lineIndex, cellIndex) == this.playerTurn) {
-                    this.chosen = [lineIndex, cellIndex];
-                    this.isChoosing = true;
-                    this.updateValidCellToMove(lineIndex, cellIndex);
-                    return;
-                }
-                if (this.isValidStep(lineIndex, cellIndex)) {
-                    this.lastStepChessboard.push(JSON.parse(JSON.stringify(this.chessboard)));
-                    this.chessboard[this.chosen[0]][this.chosen[1]] = '-';
-                    this.chessboard[lineIndex][cellIndex] = this.playerTurn;
-                    this.isChoosing = false;
-                    this.validCell = [];
-                    this.playerTurn = this.playerTurn == 'A' ? 'B' : 'A';
-                    this.isWinTheGameByLastChess(lineIndex, cellIndex);
-                    return;
-                }
-                return;
-            }
-            this.chosen = [lineIndex, cellIndex];
-            this.isChoosing = true;
-            this.updateValidCellToMove(lineIndex, cellIndex);
-        },
-        getPosInfo(lineIndex, cellIndex) {
-            return this.chessboard[lineIndex][cellIndex];
-        },
-        updateValidCellToMove(lineIndex, cellIndex) {
-            this.validCell = [];
-            for (let i = lineIndex - 1; i >= 0; i--) {
-                if (this.getPosInfo(i, cellIndex) == '-') this.validCell.push([i, cellIndex]);
-                else break;
-            }
-            for (let i = lineIndex + 1; i <= 4; i++) {
-                if (this.getPosInfo(i, cellIndex) == '-') this.validCell.push([i, cellIndex]);
-                else break;
-            }
-            for (let i = cellIndex - 1; i >= 0; i--) {
-                if (this.getPosInfo(lineIndex, i) == '-') this.validCell.push([lineIndex, i]);
-                else break;
-            }
-            for (let i = cellIndex + 1; i <= 4; i++) {
-                if (this.getPosInfo(lineIndex, i) == '-') this.validCell.push([lineIndex, i]);
-                else break;
-            }
-            for (let i = lineIndex + 1, j = cellIndex + 1; i <= 4; i++, j++) {
-                if (this.getPosInfo(i, j) == '-') this.validCell.push([i, j]);
-                else break;
-            }
-            for (let i = lineIndex - 1, j = cellIndex - 1; i >= 0; i--, j--) {
-                if (this.getPosInfo(i, j) == '-') this.validCell.push([i, j]);
-                else break;
-            }
-            for (let i = lineIndex - 1, j = cellIndex + 1; i >= 0; i--, j++) {
-                if (this.getPosInfo(i, j) == '-') this.validCell.push([i, j]);
-                else break;
-            }
-            for (let i = lineIndex + 1, j = cellIndex - 1; i <= 4; i++, j--) {
-                if (this.getPosInfo(i, j) == '-') this.validCell.push([i, j]);
-                else break;
-            }
-
-        },
-        isValidStep(lineIndex, cellIndex) {
-            return this.validCell.some(cell => {
-                return cell[0] == lineIndex && cell[1] == cellIndex;
-            });
-        },
         revertLastStep() {
             if (this.lastStepChessboard.length == 0) return;
             this.chessboard = JSON.parse(JSON.stringify(this.lastStepChessboard.pop()));
             this.playerTurn = this.playerTurn == 'A' ? 'B' : 'A';
             this.isChoosing = false;
             this.validCell = [];
-        },
-        isWinTheGameByLastChess(lineIndex, cellIndex) {
-            let lastChessOwner = this.getPosInfo(lineIndex, cellIndex);
-            let count = 1;
-            for (let i = lineIndex - 1; i >= 0; i--) {
-                if (this.getPosInfo(i, cellIndex) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = lineIndex + 1; i <= 4; i++) {
-                if (this.getPosInfo(i, cellIndex) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = cellIndex - 1; i >= 0; i--) {
-                if (this.getPosInfo(lineIndex, i) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = cellIndex + 1; i <= 4; i++) {
-                if (this.getPosInfo(lineIndex, i) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = lineIndex + 1, j = cellIndex + 1; i <= 4; i++, j++) {
-                if (this.getPosInfo(i, j) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = lineIndex - 1, j = cellIndex - 1; i >= 0; i--, j--) {
-                if (this.getPosInfo(i, j) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = lineIndex - 1, j = cellIndex + 1; i >= 0; i--, j++) {
-                if (this.getPosInfo(i, j) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
-            count = 1;
-            for (let i = lineIndex + 1, j = cellIndex - 1; i <= 4; i++, j--) {
-                if (this.getPosInfo(i, j) == lastChessOwner) {
-                    count++;
-                    if (count == 4) this.WonTheGameBy(lastChessOwner);
-                }
-                else break;
-            }
         },
         WonTheGameBy(chessOwner) {
             var x = document.getElementById("snackbar");
